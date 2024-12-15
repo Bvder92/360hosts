c7f7ae9075c0126d3044efa7d32ab4de3ba8f888
unknown <o.nouaoury@gmail.com>
2024-12-15 23:27:07 +0100

    refactor: fix routing

--- zbs-app/src/Components/Header.jsx
+++ zbs-app/src/Components/Header.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Header({ onJoinClick }) {
+function Header() {
   return (
     <div id="main">
       <div className="name">
         <h2>LOUEZ VOTRE LOGEMENT</h2>
         <h1>À TOUT MOMENT !</h1>
-        <p className="details">Gérez la location de votre logement : de l'annonce au dépôt des clés, jusqu'à votre retour chez vous.</p>
+        <p className="details">
+          Gérez la location de votre logement : de l'annonce au dépôt des clés,
+          jusqu'à votre retour chez vous.
+        </p>
         <div className="header-btns">
-          <button className="header-btn" onClick={onJoinClick}>Estimez vos revenus</button>
+          <button className="header-btn">
+            <NavLink to="/selection-region">Estimez vos revenus</NavLink>
+          </button>
         </div>
       </div>
     </div>
